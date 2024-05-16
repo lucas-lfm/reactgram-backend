072770d28e97c802ed349e8bf45d7beaa3fc7057
Lucas Ferreira Mendes <61475777+lucasifce@users.noreply.github.com>
2024-05-16 14:44:11 -0300

    :bug: fix: fix cross origin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Solve CORS
 // app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
-app.use(cors({ credentials: true, origin: "https://lucas-lfm.github.io/reactgram-frontend/" }));
+app.use(cors({ origin: "*" }));
 
 // upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
